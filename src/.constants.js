a9a22ca288f59fd0e3c738cedaa9e18762983082
Ayush22551010 <34912777+Ayush22551010@users.noreply.github.com>
2020-03-24 12:25:22 +0530

    Update .constants.js

--- src/.constants.js
+++ src/.constants.js
@@ -1,5 +1,5 @@
 module.exports = Object.freeze({
-  TOKEN: 'tokenhere', //required
+  TOKEN: '1024323932:AAHliBxtGavY5F9j8H7mNNmdks2r-L-hoDw', //required
   ARIA_SECRET: 'bhadoo', //change as per needs.
   ARIA_DOWNLOAD_LOCATION: '/app/downloads)', //-leave as it is
   INDEX_DOMAIN: 'https://workers.workers.dev/', // replace with your index domain. use https://github.com/ParveenBhadooOfficial/go-drive
@@ -8,12 +8,12 @@ module.exports = Object.freeze({
   ARIA_FILTERED_FILENAMES: [], // Files/top level directories with these substrings in the filename won't be downloaded -leave as it is
   ARIA_PORT: 8210, // Port for aria2c RPC server, if you change this here, make sure to update aria.sh as well -leave as it is
   GDRIVE_PARENT_DIR_ID:  'enter id here', // for directorty eg. https://drive.google.com/drive/folders/1p7pHhSh2yboWePmo53DnVr3xkAYiojP9 use 1p7pHhSh2yboWePmo53DnVr3xkAYiojP9
-  SUDO_USERS: [513611281],	// Telegram user IDs. These users can use the bot in any chat. required, get it using https://t.me/YourTGid_Bot
-  AUTHORIZED_CHATS: [-362157668],	// Telegram chat IDs. Anyone in these chats can use the bot. Add https://t.me/YourTGid_Bot to Group and click /id
+  SUDO_USERS: [876435153],	// Telegram user IDs. These users can use the bot in any chat. required, get it using https://t.me/YourTGid_Bot
+  AUTHORIZED_CHATS: [-1001177881184],	// Telegram chat IDs. Anyone in these chats can use the bot. Add https://t.me/YourTGid_Bot to Group and click /id
   STATUS_UPDATE_INTERVAL_MS: 12000, // A smaller number will update faster, but might cause rate limiting -leave as it is
   DRIVE_FILE_PRIVATE: {
     ENABLED: false,
-    EMAILS: ['1@gmail.com', '2@gmail.com']
+    EMAILS: ['ayushpal.3181@gmail.com', '2@gmail.com']
   }, // not required
   DOWNLOAD_NOTIFY_TARGET: {  // Information about the web service to notify on download completion. not required
     enabled: false,   // Set this to true to use the notify functionality
@@ -22,8 +22,8 @@ module.exports = Object.freeze({
     path: '/botNotify'
   },
     COMMANDS_USE_BOT_NAME: {
-      ENABLED: false,  // If true, all commands except '/list' has to have the bot username after the command.
-      NAME: "@Bhadoo1_Bot" //Enter your Bot Username here if above is set to true.
+      ENABLED: true,  // If true, all commands except '/list' has to have the bot username after the command.
+      NAME: "@RakaFamily_Bot" //Enter your Bot Username here if above is set to true.
     }
 });
 
